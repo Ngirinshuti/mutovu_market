80dfdf3ee1618d3ac48abaf4deb697b9226b3c6e
np-leon <104311071+amin-leon@users.noreply.github.com>
2025-08-19 17:10:43 +0200

    project setup

--- front_end/app/components/Card.tsx
+++ front_end/app/components/Card.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Card() {
   return (
     <div>
-        <p className='text-3xl'>Welcome to online MTV market</p>
+        <p className='text-3xl text-red-500'>Welcome to online MTV market</p>
     </div>
   )
 }
