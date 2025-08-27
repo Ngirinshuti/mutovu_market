c6267ce58f1ab58a2aa7441fff756c92e4d8b4db
Valentin <ishimwevalentin3@gmail.com>
2025-08-27 14:40:48 +0200

    adding render.com on allowed host

--- back_end/back_end/settings.py
+++ back_end/back_end/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-nq(99x07im_%ut_$edqm$&ksr*@8ra4edj^t5ut)337bnyolhx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'mutovu-market.onrender.com',
+]
 # Application definition
 AUTH_USER_MODEL = 'User.User'
 INSTALLED_APPS = [
